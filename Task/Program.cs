bd30af83ff1ae7ac904370c805766f4782e96c80
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-11-27 20:30:24 +0200

    Add array elements generator

---
+++ Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    private static int min;
+
+    static void Main()
+    {
+        int N;
+        Random rnd = new Random();
+        Console.Write("Enter array size: ");
+        bool check = int.TryParse(Console.ReadLine(), out N);
+        double[] array = new double[N];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Math.Round(rnd.NextDouble() * 200 - 100, 2);
+            Console.Write("{0}  ", array[i]);
+        }
+    }
+}
