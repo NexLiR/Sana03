774657561dfcf201baa229681dadfbd870ccb4c2
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-11-27 20:36:19 +0200

    Add negative array elements sum function

--- Task/Program.cs
+++ Task/Program.cs
@@ -15,5 +15,16 @@ class Program
             array[i] = Math.Round(rnd.NextDouble() * 200 - 100, 2);
             Console.Write("{0}  ", array[i]);
         }
+        negativeNumberSum(array);
+    }
+    static void negativeNumberSum(double[] array)
+    {
+        double sum = 0;
+        for (int i = 0;i < array.Length;i++)
+        {
+            if (array[i] < 0) sum += array[i];
+        }
+        if (sum == 0) Console.WriteLine("\nArray does not have negative elements");
+        else Console.WriteLine("\nThe sum of negative array elements: {0}", sum);
     }
 }
